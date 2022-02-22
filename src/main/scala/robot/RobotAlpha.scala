1ff46e05f0923818997a94bddbf228dd017a610b
ricardo <ricardo@txodds.com>
2022-02-22 15:38:15 +0000

    removed coursea

--- src/main/scala/robot/RobotAlpha.scala
+++ src/main/scala/robot/RobotAlpha.scala
@@ -3,19 +3,29 @@ package robot
 import cats.effect.{IO, IOApp, ExitCode}
 import cats.implicits._
 import org.http4s.implicits._
-import org.http4s.{Uri, Request, Method}
+import org.http4s.{Uri, Request, Method, Headers, Header}
 import scala.concurrent.ExecutionContext.global
 import org.http4s.blaze.client._
 import io.circe.Json
 import org.http4s.circe._
+import org.typelevel.ci._
+import org.http4s.client.middleware.GZip
 
 object RobotAlpha extends IOApp {
-  override def run(args: List[String]): IO[ExitCode] = BlazeClientBuilder[IO](global).resource.use { client =>
-    client.expect[Json](Request[IO](method = Method.GET, uri = factorsMapping))
+  override def run(args: List[String]): IO[ExitCode] = BlazeClientBuilder[IO](global).resource.map(GZip.apply[IO]()).use { client =>
+  
+    client.expect[Json](Request[IO](method = Method.GET, uri = factorsMapping).withHeaders(Headers.of(
+    Header.Raw(ci"Accept" , "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8"),
+    Header.Raw(ci"Content-type","application/json; charset=utf-8"),
+    Header.Raw(ci"Accept-Encoding","gzip")
+    )))
+  
   }.flatMap(IO.println).map(_ => ExitCode.Success)
 
+  
   val factorsMapping: Uri =uri"https://line32.bkfon-resources.com/line/factorsCatalog/tables/?lang=en&version=0"
   val uri2: Uri = uri"https://line510.bkfon-resources.com/events/list?lang=en&scopeMarket=1600&version=0"
+
 
 
 
