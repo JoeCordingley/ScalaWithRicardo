90bbd61f4bf2ddb2850edae04097e5682f627132
ricardo <ricardo@txodds.com>
2022-03-01 16:57:40 +0000

    added more stuff to .gitignore and RobotAplha looks sick

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
 
   lazy val test = List(scalaTest).map(_ % Test)
-  lazy val main = List(cats, catsEffect, http4sDsl, http4sCore, http4sClient, http4sBlazeServer, http4sBlazeClient, fs2, http4sCirce)
+  lazy val main = List(cats, catsEffect, http4sDsl, http4sCore, http4sClient, http4sBlazeServer, http4sBlazeClient, fs2, http4sCirce, Circe, CirceGeneric)
   lazy val all = main ++ test
 
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.9"
@@ -16,5 +16,7 @@ object Dependencies {
   lazy val http4sBlazeClient = "org.http4s" %% "http4s-blaze-client" % "1.0.0-M31"
   lazy val fs2 = "co.fs2" %% "fs2-core" % "3.2.5"
   lazy val http4sCirce = "org.http4s" %% "http4s-circe" % "1.0.0-M31"
+  lazy val Circe = "io.circe" %% "circe-core" % "0.14.1"
+  lazy val CirceGeneric = "io.circe" %% "circe-generic" % "0.14.1"
 
 }
--- src/main/scala/robot/RobotAlpha.scala
+++ src/main/scala/robot/RobotAlpha.scala
@@ -1,24 +1,107 @@
 package robot
 
-import cats.effect.{IO, IOApp, ExitCode}
+import cats.effect.{ExitCode, IO, IOApp}
 import cats.implicits._
-import org.http4s.implicits._
-import org.http4s.{Uri, Request, Method, Headers, Header}
-import scala.concurrent.ExecutionContext.global
+import io.circe.{Decoder, Json}
 import org.http4s.blaze.client._
-import io.circe.Json
-import org.http4s.circe._
-import org.typelevel.ci._
+import org.http4s.circe.CirceEntityCodec._
 import org.http4s.client.middleware.GZip
+import org.http4s.implicits._
+import org.http4s.{Method, Request, Uri}
+import io.circe.generic.semiauto._
+
+import java.time.LocalDateTime
 
 object RobotAlpha extends IOApp {
   override def run(args: List[String]): IO[ExitCode] = BlazeClientBuilder[IO].resource.map(GZip.apply[IO]()).use { client =>
-    client.expect[Json](Request[IO](method = Method.GET, uri = factorsMapping))
+    client.expect[Json](Request[IO](method = Method.GET, uri = uri2))
   }.flatMap(IO.println).map(_ => ExitCode.Success)
 
-  
-  val factorsMapping: Uri =uri"https://line32.bkfon-resources.com/line/factorsCatalog/tables/?lang=en&version=0"
-  val uri2: Uri = uri"https://line510.bkfon-resources.com/events/list?lang=en&scopeMarket=1600&version=0"
+  val factorsMapping: Uri =uri"https://line32.bkfon-resources.com/line/factorsCatalog/tables/?lang=en&version=0"   //<- one where we'll build the market map from
+
+  val uri2: Uri = uri"https://line510.bkfon-resources.com/events/list?lang=en&scopeMarket=1600&version=0"   //with all the events, odds and etc
+
+  //"1x2-> (factor#ofHome)(option#ofDraw)(factor#ofaway)"
+  //"moneyline 3way-> (factorofHome))(factorofaway)"
+
+  //{.league, .home, .away, .gameInfo (KO time), .ot(offer type = market name), .lineID, .oh, .oa, .od};
 
 
+  //arrays within the response from [URI2]
+  //sport -> gives us sport/league id
+
+  case class FactorsCatalog(groups: List[Group])
+  object FactorsCatalog {
+    implicit val factorsCatalogDecoder: Decoder[FactorsCatalog] = deriveDecoder
+  }
+
+  case class Group(name: String, tables: List[Table])
+  object Group{
+    implicit val groupDecoder: Decoder[Group] = deriveDecoder
+  }
+
+  case class Table(name: Option[String], rows: List[List[Element]])
+  object Table {
+    implicit val tableDecoder: Decoder[Table] = deriveDecoder
+  }
+
+  case class Factor(f: Int, v: BigDecimal, pt: Option[String])
+
+  case class CustomFactor(e: Int, factors: List[Factor])
+
+  //in the array [sports], get every element that has a field "parentId", and it's equal to "1"
+
+  case class EventData(league: String, home: String, away: String, gameInfo: LocalDateTime)
+  case class EventId(id: Int)
+  case class OddsRow(league : String, home: String, away: String, gameInfo: LocalDateTime, ot: String, oh: BigDecimal, oa: BigDecimal, od: Option[BigDecimal])
+
+  type EventMap = Map[EventId, EventData]
+  type MarketsMap = Map[FactorId, String]
+  def resolveFactors(factors : List[CustomFactor], eventMap: EventMap) : List[OddsRow] = for {
+    factor <- factors
+    oddsRow <- getOddsRow(factor, eventMap).toList
+  } yield oddsRow
+
+  def getOddsRow(factor: CustomFactor, eventMap: EventMap, marketsMap : ): Option[OddsRow] = (
+    getLeague(EventId(factor.e), eventMap),
+    getHome(EventId(factor.e), eventMap),
+    getAway(EventId(factor.e), eventMap),
+    getGameInfo(EventId(factor.e), eventMap),
+    getOt(factor.factors, marketsMap),
+    getOh,
+    getOa,
+    Some(getDraw)
+    ).mapN(OddsRow.apply)
+
+  //{921, 922, 923}  -> 1x2
+  //we can assume as true = draw is either an odds value (3way) or a points value (2way)
+  //home /away
+  //home == odd == over
+  //away == even == under
+
+  def getLeague(eventId: EventId, eventMap: EventMap): Option[String] = eventMap.get(eventId).map(_.league)
+
+  def getHome(eventId: EventId, eventMap: EventMap): Option[String] = eventMap.get(eventId).map(_.home)
+  def getAway(eventId: EventId, eventMap: EventMap): Option[String] = eventMap.get(eventId).map(_.away)
+  def getGameInfo(eventId: EventId, eventMap: EventMap): Option[LocalDateTime] = eventMap.get(eventId).map(_.gameInfo)
+
+  def getOt: Option[String] = ???
+
+  def getOh: Option[BigDecimal] = ???
+  def getOa: Option[BigDecimal] = ???
+  def getDraw: Option[BigDecimal] = ???
+
+
+
+  sealed trait Element
+  object Element {
+    case class Label(name: String) extends Element
+    case class Factor(factorId: Int) extends Element
+
+    val labelDecoder : Decoder[Element] = deriveDecoder[Label].widen
+    val factorDecoder : Decoder[Element] = deriveDecoder[Factor].widen
+    implicit val elementDecoder: Decoder[Element] = labelDecoder or factorDecoder
+
+  }
 }
+
